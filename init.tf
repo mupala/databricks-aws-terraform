bdd910e030c7ef2987be3b0bf519ef2552e715eb
MURUGESAN KARMEGAM <murugesankarmegam@MURUGESANs-MacBook-Pro.local>
2022-06-05 22:07:19 -0700

    Create Databricks E2 Workspace

---
+++ init.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    databricks = {
+      source  = "databrickslabs/databricks"
+      version = "0.3.9"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.49.0"
+    }
+  }
+}
+
+
+
+provider "aws" {
+  region                  = var.region
+  shared_credentials_file = "/Users/murugesankarmegam/.aws/credentials"
+  profile                 = "upala"
+}
+
+
+// Initialize provider in "MWS" mode to provision the new workspace.
+// alias = "mws" instructs Databricks to connect to https://accounts.cloud.databricks.com, to create
+// a Databricks workspace that uses the E2 version of the Databricks on AWS platform.
+// See https://registry.terraform.io/providers/databrickslabs/databricks/latest/docs#authentication
+provider "databricks" {
+  alias    = "mws"
+  host     = "https://accounts.cloud.databricks.com"
+  username = var.databricks_account_username
+  password = var.databricks_account_password
+}
